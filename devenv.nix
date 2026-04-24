866d1cc7d7272759c4291394b03c265614817502
Mumtahin Farabi <mfarabi619@gmail.com>
2026-04-24 00:06:34 -0400

    build(languages/python): remove module
    Signed-off-by: Mumtahin Farabi <mfarabi619@gmail.com>

--- devenv.nix
+++ devenv.nix
@@ -13,15 +13,5 @@
   languages = {
     go.enable = true;
     nix.enable = true;
-    python = {
-      enable = true;
-      version = "3.14";
-      venv = {
-        enable = true;
-        requirements = ''
-          k2l
-        '';
-      };
-    };
   };
 }
